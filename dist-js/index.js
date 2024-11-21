4f7b852107fc17505edb1eab95ad1be8a504e951
Amr Bashir <amr.bashir2015@gmail.com>
2024-11-21 13:59:59 +0000

    feat(fs): improve `readTextFile` and `readTextFileLines` performance (#1962)
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/11954486261
    
    Co-authored-by: amrbashir <amrbashir@users.noreply.github.com>

--- dist-js/index.js
+++ dist-js/index.js
@@ -442,10 +442,12 @@ async function readTextFile(path, options) {
     if (path instanceof URL && path.protocol !== 'file:') {
         throw new TypeError('Must be a file URL.');
     }
-    return await invoke('plugin:fs|read_text_file', {
+    const arr = await invoke('plugin:fs|read_text_file', {
         path: path instanceof URL ? path.toString() : path,
         options
     });
+    const bytes = arr instanceof ArrayBuffer ? arr : Uint8Array.from(arr);
+    return new TextDecoder().decode(bytes);
 }
 /**
  * Returns an async {@linkcode AsyncIterableIterator} over the lines of a file as UTF-8 string.
@@ -477,12 +479,22 @@ async function readTextFileLines(path, options) {
                     options
                 });
             }
-            const [line, done] = await invoke('plugin:fs|read_text_file_lines_next', { rid: this.rid });
-            // an iteration is over, reset rid for next iteration
-            if (done)
+            const arr = await invoke('plugin:fs|read_text_file_lines_next', { rid: this.rid });
+            const bytes = arr instanceof ArrayBuffer ? new Uint8Array(arr) : Uint8Array.from(arr);
+            // Rust side will never return an empty array for this command and
+            // ensure there is at least one elements there.
+            //
+            // This is an optimization to include whether we finished iteration or not (1 or 0)
+            // at the end of returned array to avoid serialization overhead of separate values.
+            const done = bytes[bytes.byteLength - 1] === 1;
+            if (done) {
+                // a full iteration is over, reset rid for next iteration
                 this.rid = null;
+                return { value: null, done };
+            }
+            const line = new TextDecoder().decode(bytes.slice(0, bytes.byteLength));
             return {
-                value: done ? '' : line,
+                value: line,
                 done
             };
         },
